87fbad423404b7c03d00da301263040d223e8038
vagrant <vagrant@ubuntu-chefworkstation.example.com>
2017-02-13 20:29:30 +0000

    custom_apached edited

--- cookbooks/custom_apache/recipes/default.rb
+++ cookbooks/custom_apache/recipes/default.rb
@@ -6,13 +6,13 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-package 'httpd' do
-action :intsall
-end
-service 'httpd' do
-action [ :enable, :start ]
-end
-cookbook_file '/var/www/html/index.html' do
-source 'index.html'
-mode 644
-end
+ package 'httpd' do
+ action :install
+ end
+ service 'httpd' do
+ action [ :enable, :start ]
+ end
+ cookbook_file '/var/www/html/index.html' do
+ source 'index.html'
+ mode 644
+ end
