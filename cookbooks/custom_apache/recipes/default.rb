dec3157caf7bc49ce2dc4e477f31dd10ea5d0ac8
vagrant <vagrant@ubuntu-chefworkstation.example.com>
2017-02-13 19:58:35 +0000

    created a new cookbook custom_apache

---
+++ cookbooks/custom_apache/recipes/default.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook Name:: custom_apache
+# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+package 'httpd' do
+action :intsall
+end
+service 'httpd' do
+action [ :enable, :start ]
+end
+cookbook_file '/var/www/html/index.html' do
+source 'index.html'
+mode 644
+end
